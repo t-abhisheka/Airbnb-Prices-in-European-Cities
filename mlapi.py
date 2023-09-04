7836042e4645361f047d96839b704badd0ad6c24
Abhisheka <thilinaagodage@gmail.com>
2023-09-04 23:17:30 +0530

    initial

---
+++ mlapi.py
@@ -0,0 +1,54 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
+import pandas as pd
+from fastapi import HTTPException
+app = FastAPI()
+
+class ScoreingItem(BaseModel):
+    room_shared : int
+    person_capacity : float
+    host_is_superhost : int
+    multi : int
+    biz : int
+    cleanliness_rating : float
+    guest_satisfaction_overall : float
+    bedrooms : int
+    dist : float
+    metro_dist : float
+    attr_index : float
+    attr_index_norm : float
+    rest_index : float
+    rest_index_norm : float
+    lng : float
+    lat : float
+    room_type_private_room : int
+    room_type_shared_room  : int
+    city_athens  : int
+    city_barcelona  : int
+    city_berlin  : int
+    city_budapest  : int
+    city_lisbon  : int
+    city_london  : int
+    city_paris  : int
+    city_rome  : int
+    city_vienna  : int
+    time_weekends  : int
+
+pkl_filename = "model.pkl"
+with open(pkl_filename, 'rb') as f:
+        model = pickle.load(f)
+
+
+@app.post('/')
+
+async def scoring_endpoint(item: ScoreingItem):
+    try:
+        df = pd.DataFrame([item.dict()])
+        yhat = model.predict(df)
+        return {"prediction": yhat.tolist()}
+    except Exception as e:
+        # Log the exception for debugging
+        print(f"An error occurred: {str(e)}")
+        # Raise an HTTP exception with a 500 status code
+        raise HTTPException(status_code=500, detail="Internal Server Error")
---
+++ prediction.py
@@ -0,0 +1,17 @@
+import pickle
+import pandas as pd
+import json
+
+
+def predict_price(config):
+    ##loading the model from the saved file
+    pkl_filename = "model.pkl"
+    with open(pkl_filename, 'rb') as f_in:
+        model = pickle.load(f_in)
+
+    if type(config) == dict:
+        df = pd.DataFrame(config)
+    else:
+        df = config
+
+    return model.predict(df)
